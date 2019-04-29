5e4d7145f3c2a24e568639f04896be9444562389
Kasey Markham <22298215+builtbykasey@users.noreply.github.com>
2019-04-29 16:13:07 -0400

    remove data

--- src/firebase/init.js
+++ src/firebase/init.js
@@ -3,12 +3,7 @@ import firestore from 'firebase/firestore'
 
 // Initialize Firebase
 var config = {
-  apiKey: "AIzaSyAap8OWISw_zD0Ku1eFFd62c16TOkaw6t0",
-  authDomain: "vue-smoothies-7b5ae.firebaseapp.com",
-  databaseURL: "https://vue-smoothies-7b5ae.firebaseio.com",
-  projectId: "vue-smoothies-7b5ae",
-  storageBucket: "vue-smoothies-7b5ae.appspot.com",
-  messagingSenderId: "339729682037"
+
 };
 const firebaseApp = firebase.initializeApp(config)
 
